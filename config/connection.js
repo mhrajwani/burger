e322f5b366715a4b346d95f5713455eec845b69f
mhrajwani <mo.rajwani@gmail.com>
2018-08-21 23:14:18 -0400

    Deploy

--- config/connection.js
+++ config/connection.js
@@ -14,12 +14,5 @@ else{
   database: "burgers_db"
 });
 };
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 module.exports = connection;
